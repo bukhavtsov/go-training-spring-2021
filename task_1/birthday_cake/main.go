f1fa86607b867546497a6b18975b2341eadaffc6
Artsiom Bukhautsou <artyom.bukhavtsov@gmail.com>
2021-03-20 23:23:00 +0300

    minor fixes for task description

--- task_1/birthday_cake/main.go
+++ task_1/birthday_cake/main.go
@@ -6,7 +6,7 @@ package main
 	When your friend jumps out of the cake, he/she will knock some of the candles to the floor. If the number of candles that fall is higher than 70% of total candles (x), the carpet will catch fire.
 	You will work out the number of candles that will fall from the provided string (y). You must add up the character ASCII code of each even indexed (assume a 0 based indexing) character in y, with the alphabetical position of each odd indexed character in y to give the string a total.
 	example: abc --> a=97, b=2, c=99 --> y total = 198.
-	If the carpet catches fire, return 'Fire!', if not, return 'That was close!'.
+	If the carpet catches fire, return "Fire!", if not, return "That was close!".
 
 	How to convert character to ASCII and back: https://www.socketloop.com/tutorials/golang-how-to-convert-character-to-ascii-and-back
 */
--- task_1/reverse/main.go
+++ task_1/reverse/main.go
@@ -2,7 +2,7 @@ package main
 
 /**
  * Description: Complete the solution so that it reverses the string passed into it.
- * Example: 'world'  =>  'dlrow'
+ * Example: "world"  =>  "dlrow"
  */
 
 func reverse(word string) string {
