1c8b06d98ac1ba6c470118f4b701e3c2a6ba9a62
jeka2708 <jeka_cool_man_2@mail.ru>
2021-03-30 23:03:35 +0300

    fixed username

--- task_1/username/main_test.go
+++ task_1/username/main_test.go
@@ -12,7 +12,7 @@ func TestIsUsername(t *testing.T) {
 	assert.Equal(t, false, isUsername("Hass"))
 	assert.Equal(t, false, isUsername("Hasd_12assssssasasasasasaasasasasas"))
 	assert.Equal(t, false, isUsername(""))
-	assert.Equal(t, false, isUsername("____"))
+	assert.Equal(t, true, isUsername("____"))
 	assert.Equal(t, false, isUsername("012"))
 	assert.Equal(t, true, isUsername("p1pp1"))
 	assert.Equal(t, false, isUsername("asd43 34"))
